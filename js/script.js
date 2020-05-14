56b4df66e472168b26d9d056da39de3def86c8b9
Njoki Kamau <njokifaith61@gmail.com>
2020-05-14 11:42:49 +0300

    correcting hover error still

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Form function
 $(document).ready(function(){
   $("form#form34A").submit(function(event){
     // event.preventDefault();
@@ -16,28 +16,40 @@ $(document).ready(function(){
 
 });
 
-$(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").hide();
-    $("#development").show();
+//What we do click function
 
+$(document).ready(function(){
+  $("#dev-image").click(function(){
+    $("#development").show(500);
+    $("#dev-image").hide(500);
+  });
+  $("#development").click(function(){
+    $("#dev-image").show(500);
+    $("#development").hide(500);
   });
 });
 
 $(document).ready(function(){
-  $("#design-image").click(function(){
-    $("#design-image").hide();
-    $("#design").show();
-
+  $("#des-image").click(function(){
+    $("#design").show(500);
+    $("#des-image").hide(500);
+  });
+  $("#design").click(function(){
+    $("#des-image").show(500);
+    $("#design").hide(500);
   });
 });
 
 $(document).ready(function(){
-  $("#product-image").click(function(){
-    $("#product-image").hide();
-    $("#product").show();
-
-  })
+  $("#prod-image").click(function(){
+    $("#product").show(500);
+    $("#prod-image").hide(500);
+  });
+  $("#product").click(function(){
+    $("#prod-image").show(500);
+    $("#product").hide(500);
+  });
+});
 
 //Project Portfolios Hover functions
 
